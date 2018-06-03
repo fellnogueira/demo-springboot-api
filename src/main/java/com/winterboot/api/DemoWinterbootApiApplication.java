63560d080009c986537a878ef9f71631cba40cae
fellipe <fellipe.nogueira@guep.com.br>
2018-06-03 20:15:04 -0300

    First commit

---
+++ src/main/java/com/winterboot/api/DemoWinterbootApiApplication.java
@@ -0,0 +1,12 @@
+package com.winterboot.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoWinterbootApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoWinterbootApiApplication.class, args);
+	}
+}
